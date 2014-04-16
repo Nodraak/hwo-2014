29e16cfe1b3e1074afd44fc781709ec48603f2c0
Nodraak <chardond@ece.fr>
2014-04-16 22:41:10 +0200

    basic msg formating

--- c/ft_utils.c
+++ c/ft_utils.c
@@ -6,7 +6,7 @@
 *
 *  Creation Date : 16-04-14 21:40:19
 *
-*  Last Modified : 16-04-14 21:43:03
+*  Last Modified : 16-04-14 22:03:41
 *
 *  Created By : Nodraak
 *
@@ -17,6 +17,67 @@
 
 void ft_utils_data_parse(cJSON *json)
 {
+	char *msgType = NULL;
+	cJSON *data = NULL;
+
+	msgType = cJSON_GetObjectItem(json, "msgType")->valuestring;
+	data = cJSON_GetObjectItem(json, "data");
+
+	if (strcmp(msgType, "join") == 0)
+		ft_utils_info_join_print(data);
+	else if (strcmp(msgType, "yourCar") == 0)
+		ft_utils_info_yourCar_print(data);
+	else if (strcmp(msgType, "gameInit") == 0)
+		ft_utils_track_parse(data);
+		/*ft_utils_data_raw_print("GAME INIT", data);*/
+	else if (strcmp(msgType, "gameStart") == 0)
+		printf("\t==> GAME START\n");
+	else if (strcmp(msgType, "carPositions") == 0)
+		ft_utils_data_raw_print("CAR POS", data);
+	else if (strcmp(msgType, "gameEnd") == 0)
+		ft_utils_data_raw_print("GAME END", data);
+	else
+		printf("==> unhandled : %s\n", msgType);
+}
+
+void ft_utils_info_join_print(cJSON *data)
+{
+	char *name = NULL, *key = NULL;
+
+	name = cJSON_GetObjectItem(data, "name")->valuestring;
+	key = cJSON_GetObjectItem(data, "key")->valuestring;
+
+	printf("==> JOIN\n"
+			"\tname : %s\n"
+			"\tkey : %s\n",
+			name, key);
+}
+
+
+void ft_utils_info_yourCar_print(cJSON *data)
+{
+	char *name = NULL, *color = NULL;
+
+	name = cJSON_GetObjectItem(data, "name")->valuestring;
+	color = cJSON_GetObjectItem(data, "color")->valuestring;
+
+	printf("==> YOUR CAR\n"
+			"\tname : %s\n"
+			"\tcolor : %s\n",
+			name, color);
+}
+
+void ft_utils_data_raw_print(char *type, cJSON *data)
+{
+	char *s = cJSON_Print(data);
+	printf("==> %s\n%s\n", type, s);
+	free(s);
+}
+
+void ft_utils_track_parse(cJSON *data)
+{
+
+
 
 
 }
--- c/ft_utils.h
+++ c/ft_utils.h
@@ -1,11 +1,19 @@
 #ifndef FT_UTILS_H
 #define FT_UTILS_H
 
-
-#include <sys/types.h>
+#include <stdlib.h>		/* NULL */
+#include <stdio.h>		/* printf */
+#include <sys/types.h>	/* size_t */
+#include <string.h>		/* strcmp */
 
 #include "cJSON.h"
 
 void ft_utils_data_parse(cJSON *json);
+
+void ft_utils_info_join_print(cJSON *data);
+void ft_utils_info_yourCar_print(cJSON *data);
+void ft_utils_data_raw_print(char *type, cJSON *data);
+
+void ft_utils_track_parse(cJSON *data);
 
 #endif /* FT_UTILS_H */
